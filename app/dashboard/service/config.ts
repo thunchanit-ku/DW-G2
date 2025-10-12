759f80b06d5a5baacef645d03c72b1c6fdbf6ecb
thunchanit-ku <thunchanit.t@ku.th>
2025-10-12 23:35:05 +0700

    fix my update grades page.tsx after server mess up like shit

--- app/dashboard/service/config.ts
+++ app/dashboard/service/config.ts
@@ -1,4 +1,4 @@
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:4000';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:8002';
 export const API_PREFIX = (process.env.NEXT_PUBLIC_API_PREFIX ?? 'api').replace(/^\/+|\/+$/g, '');
 
