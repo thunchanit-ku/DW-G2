a3b96dc1e71b6dab6bdbcc8819e16d7f7ca7d79f
aueaungoor <auesugoor@gmail.com>
2025-10-24 17:57:44 +0700

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   
   // Enable CORS for Next.js frontend
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001' ,  'http://localhost:8002'],
+    origin: true,
     credentials: true,
   });
 
